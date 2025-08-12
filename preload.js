c8c1f3d2ff924ece0737cbb2354670f73653332d
haro <eric@xtunes.cn>
2025-08-12 18:05:15 +0800

    修复打包electron时的数据库接口问题

--- preload.js
+++ preload.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // preload.js
 const { contextBridge, ipcRenderer } = require('electron');
 
@@ -12,6 +13,27 @@ contextBridge.exposeInMainWorld('electron', {
     // 写操作
     write: (sql, params) => ipcRenderer.invoke('db:write', sql, params),
     transaction: (queries) => ipcRenderer.invoke('db:transaction', queries),
+
+    // 统一的写接口，兼容前端 DatabaseAPI 期望
+    insert: (table, data) => {
+      const keys = Object.keys(data);
+      const values = Object.values(data);
+      const placeholders = keys.map((_, i) => `$${i + 1}`).join(', ');
+      const sql = `INSERT INTO ${table} (${keys.join(', ')}) VALUES (${placeholders})`;
+      return ipcRenderer.invoke('db:write', sql, values);
+    },
+    update: (table, id, data) => {
+      const keys = Object.keys(data);
+      const values = Object.values(data);
+      const setClause = keys.map((key, i) => `${key} = $${i + 2}`).join(', ');
+      const sql = `UPDATE ${table} SET ${setClause} WHERE id = $1`;
+      return ipcRenderer.invoke('db:write', sql, [id, ...values]);
+    },
+    delete: (table, id) => {
+      const sql = `DELETE FROM ${table} WHERE id = $1`;
+      return ipcRenderer.invoke('db:write', sql, [id]);
+    },
+    rawWrite: (sql, params) => ipcRenderer.invoke('db:write', sql, params),
     
     // 监听来自主进程的变更通知
     onChange: (callback) => {
